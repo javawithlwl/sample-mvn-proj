9ad0d1b53915e92f84dd6fcec01a6669ac4bfc17
javawithlwl <javawithlwl@gmail.com>
2023-07-06 08:32:54 +0530

    Added extra log message

--- src/main/java/com/careerit/smp/HelloMaven.java
+++ src/main/java/com/careerit/smp/HelloMaven.java
@@ -2,6 +2,7 @@ package com.careerit.smp;
 
 public class HelloMaven {
     public static void main(String[] args) {
-        System.out.println("Hello Maven");
+        System.out.println("Welcome to java with maven");
+        System.out.println("We are going push code to github");
     }
 }
