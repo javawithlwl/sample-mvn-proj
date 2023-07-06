2c34ff383fe74d6fe4942b3e5411e9463df1f63e
javawithlwl <javawithlwl@gmail.com>
2023-07-06 08:41:14 +0530

    Added sum of two numbers

---
+++ src/main/java/com/careerit/smp/SumOfTwoNumbers.java
@@ -0,0 +1,9 @@
+package com.careerit.smp;
+public class SumOfTwoNumbers{
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = a+b;
+        System.out.println("The sum of "+a+" and "+b+" is "+c);
+    }
+}
